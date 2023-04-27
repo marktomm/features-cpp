6c4bc34a1b5033e235960b35df16c5e8335970e1
Mark Tomm <mark.tomm@martem.ee>
2023-04-27 21:55:32 +0300

    asm_prettify do lib also with 3rd arg set to "lib"

--- asm_prettify.sh
+++ asm_prettify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ARG1=$1
+ARG2=$2
+ARG3=$3
 
-asmfile=builddir/${ARG1}/${ARG1}_main.s
+asmfile="builddir/${ARG1}/${ARG1}_$( [[ -n "${ARG3}" ]] && echo "${ARG3}" || echo "main" )${ARG2}.s"
 
 awk '{print NR, $0}' ${asmfile}
