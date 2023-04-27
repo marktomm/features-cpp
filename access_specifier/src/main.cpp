2ec4d3b320aefb2d874b986c696a9a0da611e31b
Mark Tomm <mark.tomm@martem.ee>
2023-04-27 20:02:40 +0300

    access_specifier: add asm build and copying op to code

--- access_specifier/src/main.cpp
+++ access_specifier/src/main.cpp
@@ -1,3 +1,16 @@
 #include "lib.h"
+#include <iostream>
 
-int main() { return 0; }
+int main() {
+    using namespace access_specifier;
+    using namespace std;
+    A a;
+    cout << a.a;
+    A2 a2;
+    cout << a2.a;
+
+    [[maybe_unused]] A aa = a;
+    [[maybe_unused]] A2 aa2 = a2;
+
+    return 0;
+}
