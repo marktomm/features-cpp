5a8e7c0435af89a2a0f08e51a1d725efa22a200e
Mark Tomm <mark.tomm@martem.ee>
2023-04-29 23:55:56 +0300

    minor script

--- bench_prettify.sh
+++ bench_prettify.sh
@@ -15,5 +15,7 @@ ARG2=$2
 
 runFile="builddir/${ARG1}/${ARG1}_benchmark_google_$( [[ -n "${ARG2}" && "${ARG2}" == 'opt' ]] && echo "" || echo "no_" )opt"
 
+./_build.sh
+
 ${runFile} | sed -E 's/(^.*[^0-9])[0-9]*$/\1/'
 
