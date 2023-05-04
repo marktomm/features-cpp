21b487f1398d38731c8c7bad4282ea98208956d1
Mark Tomm <mark.tomm@martem.ee>
2023-05-04 18:08:50 +0300

    add to common

--- common/include/lib.h
+++ common/include/lib.h
@@ -2,15 +2,33 @@
 #define MACRO_common_LIB_H
 
 #include <cstdint>
+#include <vector>
 
 namespace common {
 
-class A {
-public:
-};
+using vb = std::vector<bool>;
+using vu32 = std::vector<uint32_t>;
 
 // GEN_PROTO_BEGIN
+int getRandom12();
+int getRandom13();
+std::size_t GetNextGlobalIndex();
+vb GetGlobalRandBoolVec();
+void emptyFn();
+void emptyFnInt();
+vu32 GetGlobalRandIntVec();
 // GEN_PROTO_END
+
+template<class T>
+void rreorder(std::vector<T>& v, vu32 const& order) {
+    for (uint32_t s = 1, d; s < order.size(); ++s) {
+        for (d = order[s]; d < s; d = order[d])
+            ;
+        if (d == s)
+            while (d = order[d], d != s)
+                swap(v[s], v[d]);
+    }
+}
 
 }; // namespace common
 
